c67bf80388df16d5eb7327498b669f6effd87915
unknown <sswramadhan@gmail.com>
2023-04-08 17:54:09 +0700

    popular movie show

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img
+            className="Movie-image"
+            src="${Process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}"
+          />
+          <div className="Movie-date">{movie.release_data}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
   const search = (q) => {
     console.log({ q });
   };
@@ -9,18 +35,13 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>Moview</h1>
+        <input
+          placeholders="Find Movie Review..."
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
         <div className="Movie-container">
-          <input
-            placeholders="Find Movie Review..."
-            className="Movie-search"
-            onChange={({ target }) => search(target.value)}
-          />
-          <div className="Movie-wrapper">
-            <div className="Movie-title"></div>
-            <img className="Movie-image" />
-            <div className="Movie-date"></div>
-            <div className="Movie-rate"></div>
-          </div>
+          <PopularMoviesList />
         </div>
       </header>
     </div>
