542c660f44eb61dda94258d335e268eb3563bb43
unknown <sswramadhan@gmail.com>
2023-04-08 17:16:02 +0700

    Inisiasi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const search = (q) => {
+    console.log({ q });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Moview</h1>
+        <div className="Movie-container">
+          <input
+            placeholders="Find Movie Review..."
+            className="Movie-search"
+            onChange={({ target }) => search(target.value)}
+          />
+          <div className="Movie-wrapper">
+            <div className="Movie-title"></div>
+            <img className="Movie-image" />
+            <div className="Movie-date"></div>
+            <div className="Movie-rate"></div>
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
